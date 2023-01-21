84b0d3e058c52fddcce0506ffc6c26792dc92f16
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2023-01-21 18:20:46 +0300

    gh

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ const projectName = "<repository-name>";
 const config = {
   title: 'TON Metaspace',
   tagline: 'Open source metaverse for The Open Network',
-  url: 'https://${organizationName}.github.io',
-  baseUrl = '/${projectName}/',
+  url: 'https://tonmetaspace.github.io',
+  baseUrl = '/metaspace.ton/',
   onBrokenLinks: 'throw',
   onBrokenMarkdownLinks: 'warn',
   favicon: 'img/favicon.ico',
@@ -19,8 +19,7 @@ const config = {
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'tonmetaspace', // Usually your GitHub org/user name.
-  projectName: 'metaspace.ton', // Usually your repo name.
+
 
   // Even if you don't use internalization, you can use this field to set useful
   // metadata like html lang. For example, if your site is Chinese, you may want
