7d099401005b99a4ccea6de1bfe7931bc10bc92b
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2023-01-31 15:27:19 +0300

    pull

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -14,7 +14,7 @@ const config = {
   url: 'https://tonmetaspace.github.io/',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: 'metaspace.ton/',
+  baseUrl: '/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
