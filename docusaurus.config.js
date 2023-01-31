8ec8a9a2a26a907839bc9f1619daabf03d9deadf
Roman Inozemtsev <inozemtsev_roman@mail.ru>
2023-01-31 15:23:53 +0300

    upd

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -11,7 +11,7 @@ const config = {
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
-  //vurl: 'https://your-docusaurus-test-site.com',
+  url: 'https://tonmetaspace.github.io/metaspace.ton',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: 'metaspace.ton/',
